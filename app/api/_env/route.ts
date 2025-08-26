909e4fb3551dba425d2a332e419412dca5313800
AlbertoRMelgoza <am@albertormelgoza.com>
2025-08-26 20:54:34 +1000

    Create route.ts

---
+++ app/api/_env/route.ts
@@ -0,0 +1,10 @@
+import { NextResponse } from "next/server";
+export const runtime = "nodejs";
+
+export async function GET() {
+  return NextResponse.json({
+    STRIPE_SECRET_KEY: !!process.env.STRIPE_SECRET_KEY,
+    STRIPE_SECRET: !!process.env.STRIPE_SECRET,
+    VERCEL_ENV: process.env.VERCEL_ENV || null
+  });
+}
